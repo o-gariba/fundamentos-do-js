f6609b18dcc929c324aebcf9b6fc0661059ba53f
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-02-10 09:42:34 -0300

    array bidimensional e método concat

---
+++ concat.js
@@ -0,0 +1,7 @@
+const sala1 = ['joao', 'marcelo', 'ricardo'];
+
+const sala2 = ['rogerio', 'juca'];
+
+const salaUnificada = sala1.concat(sala2);
+
+console.log(salaUnificada)
---
+++ lista-duas-dimensoes.js
@@ -0,0 +1,15 @@
+const nomes = ['Pedro', 'Carolina', 'Lucas'];
+
+const idades = [24,26,24];
+
+let listaNomeEIdade = [nomes, idades]
+
+console.log(listaNomeEIdade)
+
+// Acessando os dados no array bidimensional. P.e idade da Carolina:
+
+console.log(listaNomeEIdade[1][1])
+
+// Quais as idades dentro do array listaNomeEIdade?
+
+console.log(listaNomeEIdade[1])
