90af00b941c90f19cd9a7f3a85b2b6ddbbafba74
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-02-22 09:48:56 -0300

    Adicionada aula de composição de obetos.

---
+++ Objetos/compondo-objetos.js
@@ -0,0 +1,16 @@
+const meusDados = {
+    nome : "Pedro",
+    idade : 24,
+    corDoCabelo : "castanho",
+    profissao : "estudante",
+    cpf: "92873127836"
+}
+
+meusDados.cursosAtivos = {
+    nome: ["Fundamentos em javascript: Objetos", "Layouts responsivos"],
+    cargaHoraria: [20, 16]
+}
+
+console.log(meusDados);
+
+// Para alterarmos valores de objetos em objetos, faço o mesmo procedimento de antes, acessando as chaves atráves de pontos '.'
