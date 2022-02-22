d7d9a9a1ddc6da03eeb27a1a4255bf738c78941e
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-02-22 10:31:54 -0300

    Aula de array de objetos dentro de objetos add.

--- Objetos/compondo-objetos.js
+++ Objetos/compondo-objetos.js
@@ -6,11 +6,35 @@ const meusDados = {
     cpf: "92873127836"
 }
 
-meusDados.cursosAtivos = {
-    nome: ["Fundamentos em javascript: Objetos", "Layouts responsivos"],
-    cargaHoraria: [20, 16]
-}
+// meusDados.cursosAtivos = {
+//     nome: ["Fundamentos em javascript: Objetos", "Layouts responsivos"],
+//     cargaHoraria: [20, 16]
+// }
 
 console.log(meusDados);
 
 // Para alterarmos valores de objetos em objetos, faço o mesmo procedimento de antes, acessando as chaves atráves de pontos '.'
+
+// Como podemos lidar com uma lista de objetos dentro de um objeto. Vamos usar um array, da seguinte maneira:
+
+meusDados.cursosAtivos = [{
+    nome: "Fundamentos em javascript",
+    cargaHoraria: 20
+}]
+
+// IMPORTANTE, SABENDO QUE VOU TER UM ARRAY DE OBJETOS, USEI A NOTAÇÃO DE ARRAY [] PARA DAR A PRIMEIRA INSTRUÇÃO AO OBJETO MÃE
+
+meusDados.cursosAtivos.push({
+    nome: "Layout responsivo",
+    cargaHoraria: 16
+})
+
+// Agora posso usar métodos próprias de array, como o push()
+
+console.log(meusDados);
+
+// Posso também usar o método filter dentro do array de objetos, por exemplo
+
+const cursoMaisRapido = meusDados.cursosAtivos.filter(curso => curso.cargaHoraria === 16);
+
+console.log(cursoMaisRapido);
